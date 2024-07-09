3a7c9e392c77c7b8c3c117d39f0d769bb2f36aa5
thd2020 <1119523246@qq.com>
2024-07-09 17:07:13 +0800

    Add Authorizations and more patient consultations to services and controllers

--- pas-main/src/main/java/com/thd2020/pasmain/dto/RelatedIdsResponse.java
+++ pas-main/src/main/java/com/thd2020/pasmain/dto/RelatedIdsResponse.java
@@ -2,18 +2,18 @@ package com.thd2020.pasmain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RelatedIdsResponse {
     private Long patientId;
     private List<Long> medicalRecordIds;
     private List<Long> surgeryAndBloodTestIds;
     private List<Long> ultrasoundScoreIds;
-
-    // Constructor, getters and setters...
 }
